1b43f5708fec954e6a55c596979c11dec09a6c9e
Matteo Collina <hello@matteocollina.com>
2018-06-02 14:16:02 +0200

    Updated dependencies

--- test.js
+++ test.js
@@ -1,25 +1,36 @@
 'use strict'
 
-var mongoEmitter = require('./')
+var mongodb = require('mongodb')
+var MongoClient = mongodb.MongoClient
+var MongoEmitter = require('./')
 var test = require('tape').test
 var abstractTests = require('mqemitter/abstractTest.js')
 var clean = require('mongo-clean')
-var url = 'mongodb://127.0.0.1/mqemitter-test'
+var dbname = 'mqemitter-test'
+var url = 'mongodb://127.0.0.1/'
 
-clean(url, function (err, db) {
+MongoClient.connect(url, { w: 1 }, function (err, client) {
   if (err) {
     throw err
   }
 
-  db.close()
+  var db = client.db(dbname)
 
-  abstractTests({
-    builder: function (opts) {
-      opts = opts || {}
-      opts.url = url
+  clean(db, function (err) {
+    if (err) {
+      throw err
+    }
 
-      return mongoEmitter(opts)
-    },
-    test: test
+    client.close()
+
+    abstractTests({
+      builder: function (opts) {
+        opts = opts || {}
+        opts.url = url
+
+        return MongoEmitter(opts)
+      },
+      test: test
+    })
   })
 })
