0d064864b0987143f82c86abeedcb808752e2e8d
조형재 <jhj12010@gmail.com>
2019-10-29 16:33:59 +0900

    Test for mongo option and database option

--- test.js
+++ test.js
@@ -32,5 +32,58 @@ MongoClient.connect(url, { useNewUrlParser: true, w: 1 }, function (err, client)
       },
       test: test
     })
+
+    test('with default database name', function (t) {
+      t.plan(2)
+
+      var mqEmitterMongoDB = MongoEmitter({
+        url: url
+      })
+
+      t.timeoutAfter(3000)
+      setTimeout(function () {
+        t.equal(mqEmitterMongoDB._db.databaseName, dbname)
+        t.ok(true, 'database name is default db name')
+        t.end()
+        mqEmitterMongoDB.close()
+      }, 2500)
+    })
+
+    test('with database option', function (t) {
+      t.plan(2)
+
+      var mqEmitterMongoDB = MongoEmitter({
+        url: url,
+        database: 'test-custom-db-name'
+      })
+
+      t.timeoutAfter(3000)
+      setTimeout(function () {
+        t.equal(mqEmitterMongoDB._db.databaseName, 'test-custom-db-name')
+        t.ok(true, 'database name is custom db name')
+        t.end()
+        mqEmitterMongoDB.close()
+      }, 2500)
+    })
+
+    test('with mongodb options', function (t) {
+      t.plan(3)
+
+      var mqEmitterMongoDB = MongoEmitter({
+        url: url,
+        mongo: {
+          keepAlive: false,
+          poolSize: 9
+        }
+      })
+      t.timeoutAfter(3000)
+      setTimeout(function () {
+        t.equal(mqEmitterMongoDB._db.serverConfig.s.clonedOptions.keepAlive, false)
+        t.equal(mqEmitterMongoDB._db.serverConfig.s.clonedOptions.poolSize, 9)
+        t.ok(true, 'database name is default db name')
+        t.end()
+        mqEmitterMongoDB.close()
+      }, 2500)
+    })
   })
 })
